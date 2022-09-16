29dfbc803f691fa550e5d96580f3a3e37d73031a
7i7o <mparij@gmail.com>
2022-09-16 13:22:53 -0300

    build: added profileId to interface

--- src/interfaces/IPobEscrow.sol
+++ src/interfaces/IPobEscrow.sol
@@ -8,53 +8,61 @@ interface IPobEscrow {
     /** EVENTS */
 
     /** @notice event to signal that a new publication has been published
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      *  @param price Price of the item to sell
      */
-    event Sell(uint256 pubId, uint256 price);
+    event Sell(uint256 profileId, uint256 pubId, uint256 price);
 
     /** @notice event to signal that a publication received an offer to buy
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      *  @param commissioner address of the user that mirrored the publication or marketplace if 0x0
      */
-    event Buy(uint256 pubId, address commissioner);
+    event Buy(uint256 profileId, uint256 pubId, address commissioner);
 
     /** @notice event to signal that a buy has been canceled by the seller
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      */
-    event BuyCanceled(uint256 pubId);
+    event BuyCanceled(uint256 profileId, uint256 pubId);
 
     /** @notice event to signal that a buy has been confirmed by the buyer
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      */
-    event BuyConfirmed(uint256 pubId);
+    event BuyConfirmed(uint256 profileId, uint256 pubId);
 
 
     /** FUNCTIONS */
 
     /** @notice function to sell an item 
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      *  @param price Price of the item to sell
      *  @dev seller will be the msg.sender
      */
-    function sell(uint256 pubId, uint256 price) external;
+    function sell(uint256 profileId, uint256 pubId, uint256 price) external;
 
     /** @notice function to buy an item
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      *  @param commissioner address of the user that mirrored the publication or marketplace if 0x0
      *  @dev buyer will be the msg.sender
      */
-    function buy(uint256 pubId, address commissioner) external payable;
+    function buy(uint256 profileId, uint256 pubId, address commissioner) external payable;
 
     /** @notice function to cancel an item bought and return the value locked
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      *  @dev reverts if msg.sender is not the seller 
      */
-    function cancelBuy(uint256 pubId) external;
+    function cancelBuy(uint256 profileId, uint256 pubId) external;
 
     /** @notice function to signal a confirmed buy and unlock the value for seller and commissioner
+     *  @param profileId Id of the Lens Profile
      *  @param pubId Id of the Lens Publication
      *  @dev reverts if msg.sender is not the buyer
      */
-    function confirmBuy(uint256 pubId) external;
+    function confirmBuy(uint256 profileId, uint256 pubId) external;
 }
